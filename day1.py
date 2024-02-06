7f811c82b6d17fcbc0abfadc9cda40e8aacc3c7f
philipakomolafe <akomolafephilip@gmail.com>
2024-02-06 10:58:08 +0100

    1

---
+++ day1.py
@@ -0,0 +1,18 @@
+#DAY1 of HACKERRANK_30_DAYS_CHALLENGE
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i_1 = int(input().strip())
+d_1 = float(input().strip())
+s_1 = input().strip()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + i_1)
+# Print the sum of the double variables on a new line.
+print(d + d_1)
+# Concatenate and print the String variables on a new line
+print(s + s_1)
+# The 's' variable above should be printed first.
